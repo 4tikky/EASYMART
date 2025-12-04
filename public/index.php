1b71156bc41cd2eccefb1f21c30b5520e3d5c811
4tikky <atikaleng@gmail.com>
2025-12-05 02:36:06 +0700

    Percantik tampilan login,register

--- public/index.php
+++ public/index.php
@@ -17,4 +17,10 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$response = $app->handleRequest(
+    Request::capture()
+);
+
+if ($response !== null) {
+    $response->send();
+}
